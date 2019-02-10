b5380938b29a573598a01261361f4ef2958bdda0
Kate Shaitarova <eshaytarova@gmail.com>
2019-02-11 11:39:23 +1300

    Updates

---
+++ precache-manifest.0fa96dde355c7c93d1018c1799026fb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "90ca650cf9a608993268b0f560d6bde4",
+    "url": "/my-shopping-list/static/media/shopping.90ca650c.jpg"
+  },
+  {
+    "revision": "b18781a53a8803616398",
+    "url": "/my-shopping-list/static/js/runtime~main.b18781a5.js"
+  },
+  {
+    "revision": "21fc860b3036d6d74730",
+    "url": "/my-shopping-list/static/js/main.21fc860b.chunk.js"
+  },
+  {
+    "revision": "884aa76bcbac58742162",
+    "url": "/my-shopping-list/static/js/1.884aa76b.chunk.js"
+  },
+  {
+    "revision": "21fc860b3036d6d74730",
+    "url": "/my-shopping-list/static/css/main.d4e46036.chunk.css"
+  },
+  {
+    "revision": "884aa76bcbac58742162",
+    "url": "/my-shopping-list/static/css/1.99a46de7.chunk.css"
+  },
+  {
+    "revision": "b72a86ee6581e5664e9b34f1943d4c4d",
+    "url": "/my-shopping-list/index.html"
+  }
+];
